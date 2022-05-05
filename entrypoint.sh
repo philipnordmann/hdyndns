28c244e62bdef866e3db09611cf1ab7ce05a330e
philipnordmann <nordmann.philip@googlemail.com>
2022-05-05 18:40:27 +0200

    removed delete option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 while true; do
     /opt/hdyndns/hdyndns.py \
-        --name ${SUBDOMAIN_NAMES} \
+        --names ${SUBDOMAIN_NAMES} \
         --hetzner-token ${HETZNER_TOKEN} \
         --hetzner-zone-id ${HETZNER_ZONE_ID}
     sleep ${UPDATE_RATE:-60}
